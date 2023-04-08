abbae6326cda21bf1566975075bc81ff0ca67102
许佳豪 <10351874+xujiahao29@user.noreply.gitee.com>
2023-04-08 14:52:34 +0800

    init

---
+++ app/constant.ts
@@ -0,0 +1,7 @@
+export const OWNER = "黄孝成的chatgpt网站";
+export const REPO = "ChatGPT";
+export const REPO_URL = `#`;
+export const ISSUE_URL = `#`;
+export const UPDATE_URL = `#`;
+export const FETCH_COMMIT_URL = `#`;
+export const FETCH_TAG_URL = `#`;
